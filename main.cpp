1c66203d4083de45e7843e3582ac0cdc4769b4e0
Archer <ArcherC521@users.noreply.github.com>
2017-09-27 23:08:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -68,6 +68,56 @@ void depth2normal(
 	cv::imwrite(normalMapFile, normalMap);
 }
 
+void depth2normal(
+	const Eigen::Matrix4f &I,
+	const Eigen::MatrixXf &R,
+	const Eigen::Vector4f &T,
+	const std::string &depthMapFile,
+	const std::string &normalMapFile
+) {
+	cv::Mat depthMap = cv::imread(depthMapFile, cv::IMREAD_UNCHANGED);
+
+	std::vector<std::vector<Eigen::Vector3f>> coor(depthMap.rows);
+	for (int row = 0; row < depthMap.rows; ++row) coor[row].reserve(depthMap.cols);
+	for (int u = 0; u < depthMap.cols; ++u)
+		for (int v = 0; v < depthMap.rows; ++v) {
+			if (0 == depthMap.at<ushort>(v, u)) {
+				coor[v][u] = Eigen::Vector3f(0.f, 0.f, 0.f);
+				continue;
+			}
+			float d = static_cast<float>(depthMap.at<ushort>(v, u)) / 1000.f;
+			Eigen::MatrixXf A = I * R;
+			Eigen::Vector4f B = Eigen::Vector4f(d * (u + 1), d * (v + 1), d, 1.f) - I * T;
+			coor[v][u] = A.colPivHouseholderQr().solve(B);
+		}
+
+	cv::Mat normalMap = cv::Mat::zeros(depthMap.rows, depthMap.cols, CV_32FC3);
+	for (int u = 0; u < depthMap.cols; ++u)
+		for (int v = 0; v < depthMap.rows; ++v) {
+			Eigen::Vector3f normal;
+
+			Eigen::Vector3f vecUp, vecDown, vecLeft, vecRight;
+			if (0 < v) vecUp = coor[v - 1][u] - coor[v][u];
+			if (v < depthMap.rows - 1) vecDown = coor[v + 1][u] - coor[v][u];
+			if (0 < u) vecLeft = coor[v][u - 1] - coor[v][u];
+			if (u < depthMap.cols - 1) vecRight = coor[v][u + 1] - coor[v][u];
+
+			if (0 == u && 0 == v) normal = vecDown.cross(vecRight).normalized();
+			else if (0 == u && depthMap.rows - 1 == v) normal = vecRight.cross(vecUp).normalized();
+			else if (depthMap.cols - 1 == u && 0 == v) normal = vecLeft.cross(vecDown).normalized();
+			else if (depthMap.cols - 1 == u && depthMap.rows - 1 == v) normal = vecUp.cross(vecLeft).normalized();
+			else if (0 == u) normal = (vecDown.cross(vecRight).normalized() + vecRight.cross(vecUp).normalized()).normalized();
+			else if (depthMap.cols - 1 == u) normal = (vecUp.cross(vecLeft).normalized() + vecLeft.cross(vecDown).normalized()).normalized();
+			else if (0 == v) normal = (vecLeft.cross(vecDown).normalized() + vecDown.cross(vecRight).normalized()).normalized();
+			else if (depthMap.rows - 1 == v) normal = (vecRight.cross(vecUp).normalized() + vecUp.cross(vecLeft).normalized()).normalized();
+			else normal = (vecUp.cross(vecLeft).normalized() + vecDown.cross(vecRight).normalized()).normalized();
+			
+			normalMap.at<cv::Vec3f>(v, u) = cv::Vec3f((1 - normal[2]) / 2.f, (1 - normal[1]) / 2.f, (1 - normal[0]) / 2.f);
+		}
+	normalMap.convertTo(normalMap, CV_8U, 255);
+	cv::imwrite(normalMapFile, normalMap);
+}
+
 int main(int argc, char **argv) {
 	std::string depthMapFile("../depth.png");
 	std::string poseFile("../pose.txt");
@@ -93,7 +143,8 @@ int main(int argc, char **argv) {
 		std::ifstream is(poseFile);
 		for (int i = 0; i < 4; ++i) is >> R(i, 0) >> R(i, 1) >> R(i, 2) >> T[i];
 
-		depth2normal(I, R, T, depthMapFile, modelFile, modelnFile, normalMapFile);
+		// depth2normal(I, R, T, depthMapFile, modelFile, modelnFile, normalMapFile);
+		depth2normal(I, R, T, depthMapFile, normalMapFile);
 	}
 	catch (const std::exception &e) {
 		std::cout << e.what() << std::endl;
